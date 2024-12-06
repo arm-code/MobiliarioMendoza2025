ecf9c57686597f94c38cc5cc3fef28ce0e63bb38
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2024-12-06 14:10:24 -0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ import { ProductosPage } from '../rentar/productos/ProductosPage';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to='/home'/> }/>
+      <Route path='/*' element={<Navigate to='/home'/> }/>
       <Route path='/home' element={<Home/>}/>
       <Route path='/rentar' element={<ProductosPage/>}/>
     </Routes>
